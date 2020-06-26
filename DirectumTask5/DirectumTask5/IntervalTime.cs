88ecc4dffc1dacac4f1993a54ee9ab7c06e6505d
mcldays <mcldays@gmail.com>
2020-06-26 17:12:10 +0400

    6 задачка, не знаю как по другому делать без регулярок

---
+++ DirectumTask5/DirectumTask5/IntervalTime.cs
@@ -0,0 +1,44 @@
+﻿namespace DirectumTask5
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Defines the <see cref="IntervalTime" />.
+    /// </summary>
+    internal class IntervalTime
+    {
+        /// <summary>
+        /// The RecordsCount.
+        /// </summary>
+        /// <param name="path">The path<see cref="string"/>.</param>
+        /// <param name="start">The start<see cref="DateTime"/>.</param>
+        /// <param name="end">The end<see cref="DateTime"/>.</param>
+        /// <returns>The <see cref="int"/>.</returns>
+        public int RecordsCount(string path, DateTime start, DateTime end)
+        {
+            int result = 0;
+            Regex regular = new Regex(@"\A\d{2}\.\d{2}\.\d{4}");
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (regular.Match(line).Success)
+                    {
+                        DateTime parseDate = DateTime.ParseExact(regular.Match(line).Value, "dd.MM.yyyy", null);
+
+                        if (parseDate >= start && parseDate <= end)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DirectumTask5/DirectumTask5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DirectumTask5
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IntervalTime kek = new IntervalTime();
+            kek.RecordsCount("ClientConnectionLog.log", new DateTime(2007, 12, 7), new DateTime(2007, 12, 17));
+        }
+    }
+}
