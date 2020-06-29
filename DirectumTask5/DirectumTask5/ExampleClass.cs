fe961b691582682c689623f270e932017c38252d
mcldays <mcldays@gmail.com>
2020-06-29 15:23:25 +0400

    Доп задание

---
+++ DirectumTask5/DirectumTask5/ExampleClass.cs
@@ -0,0 +1,22 @@
+﻿namespace DirectumTask5
+{
+    /// <summary>
+    /// Defines the <see cref="ExampleClass" />.
+    /// </summary>
+    internal class ExampleClass
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExampleClass"/> class.
+        /// </summary>
+        /// <param name="name">The name<see cref="string"/>.</param>
+        public ExampleClass(string name)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
--- DirectumTask5/DirectumTask5/Program.cs
+++ DirectumTask5/DirectumTask5/Program.cs
@@ -13,9 +13,10 @@
         /// <param name="args">The args<see cref="string[]"/>.</param>
         internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            IntervalTime kek = new IntervalTime();
-            kek.RecordsCount("ClientConnectionLog.log", new DateTime(2007, 12, 7), new DateTime(2007, 12, 17));
+            ExampleClass example = new ExampleClass("test");
+            Console.WriteLine(example.ToString());
+            IntervalTime time = new IntervalTime();
+            time.RecordsCount("ClientConnectionLog.log", new DateTime(2007, 12, 7), new DateTime(2007, 12, 17));
         }
     }
 }
