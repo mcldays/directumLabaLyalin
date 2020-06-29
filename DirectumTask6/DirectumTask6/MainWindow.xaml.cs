e6d9dc67c4631e05cb1726b1549ec2c4620ce6d2
mcldays <mcldays@gmail.com>
2020-06-29 14:42:35 +0400

    7 задание

---
+++ DirectumTask6/DirectumTask6/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace DirectumTask6
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The UnpackStreamToTextBox.
+        /// </summary>
+        /// <param name="fileName">The fileName<see cref="string"/>.</param>
+        /// <param name="textBox">The TextBox<see cref="RichTextBox"/>.</param>
+        public static void UnpackStreamToTextBox(string fileName, RichTextBox textBox)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                using (GZipStream unpack = new GZipStream(fs, CompressionMode.Decompress))
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    unpack.CopyTo(memory);
+                    TextRange document = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+                    document.Load(memory, DataFormats.Rtf);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// The Button_Click.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="RoutedEventArgs"/>.</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UnpackStreamToTextBox(@".\q2.rtf.gz", this.TextBox);
+        }
+    }
+}
